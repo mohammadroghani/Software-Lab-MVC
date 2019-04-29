1b701a527385ac8a1a2add894c69a4bfd34f0263
Mohammad <mohammadroghani43@gmail.com>
2019-04-29 19:16:31 +0430

    fix bugs

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import selab.mvc.models.DataContext;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        App app = new App(4614);
+        App app = new App(9324);
         DataContext dataContext = new DataContext();
 
         app.addController("/", new IndexController());
